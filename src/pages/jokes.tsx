fe8b9414d7c1e1bcf39687effc6efff9ddff2f3c
Robin Reyniers <Robinreyniers@hotmail.com>
2023-02-16 21:30:38 +0100

    Jokes

--- src/pages/jokes.tsx
+++ src/pages/jokes.tsx
@@ -1,13 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 
 function jokes() {
+  const [joke, setJoke] = useState("");
   useEffect(() => {
     fetch("https://api.chucknorris.io/jokes/random")
       .then((res) => res.json())
       .then((data) => console.log(data));
   }, []);
-  return <div>{}</div>;
+  return <div>{joke}</div>;
 }
 
 export default jokes;
