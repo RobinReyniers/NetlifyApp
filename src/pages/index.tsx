1bc75e25bd0187de1c8a4b53981699495631d3cf
Robin Reyniers <Robinreyniers@hotmail.com>
2023-03-02 20:52:59 +0100

    test 2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,10 +10,7 @@ export default function Home({ pets }: any) {
   return (
     <ul>
       {pets.map((pet: any) => (
-        <li key={pet._id}>
-          {pet?.name}
-          {pet?.name}
-        </li>
+        <li key={pet._id}>{pet?.name}</li>
       ))}
     </ul>
   );
